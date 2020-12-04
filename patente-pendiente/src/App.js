1b545960ac422c9331c24f4a069d60fda5da2563
Smansilla98 <56280563+Smansilla98@users.noreply.github.com>
2020-12-03 23:51:08 -0300

    react

---
+++ patente-pendiente/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4> Patente Pendiente </h4>
+        <p>
+         Beta - Coderhouse
+        </p>
+        </header>
+    </div>
+  );
+}
+
+export default App;
